6f3d79c44c0ad04973f71c7b7bd1727c6ac2a7b0
Abhishek <abhishekpadhi69@gmail.com>
2023-03-27 12:19:19 +0530

    Added the drop downs in signup and edit user

---
+++ app/helpers/city_helper.rb
@@ -0,0 +1,77 @@
+module CityHelper
+    def cities
+        [
+          ['Bengaluru'],
+          ['Mumbai'],
+          ['Chennai'],
+          ['Hyderabad'],
+          ['Pune'],
+          ['Mysore'],
+          ['Bhopa'],
+          ['Indore'],
+          ['Srinagar'],
+          ['Shimla'],
+          ['Chandigarh'],
+          ['Delhi'],
+          ['Dehradun'],
+          ['Assam'],
+          ['Lucknow'],
+          ['Noida'],
+          ['Gurgaon'],
+          ['Jaipur'],
+          ['Kota'],
+          ['Ranchi'],
+          ['Kanpur'],
+          ['Kolkata'],
+          ['Bhubaneshwar'],
+          ['Raipur'],
+          ['Darjeeling'],
+          ['Shimla'],
+          ['Chandigarh'],
+          ['Delhi'],
+          ['Visakhapatnam'],
+          ['Nagpur'],
+          ['Patna'],
+          ['Ahmedabad'],
+       
+        ]
+    end
+
+    def states
+      [
+        [' Andhra Pradesh',],
+        ['Arunachal Pradesh'],
+        ['Assam'],
+        ['Bihar'],
+        ['Chhattisgarh',],
+        ['Goa'],
+        ['Gujrat'],
+        ['Haryana'],
+        ['Himachal Pradesh'],
+        ['Jammu and Kashmir'],
+        
+        ['Jharkhand'],
+        ['Karnataka'],
+        
+        ['Kerala'],
+        ['Madhya Pradesh'],
+        ['Maharashtra'],
+        ['Manipur'],
+        ['Meghalaya'],
+        ['Mizoram'],
+        ['Nagaland'],
+        ['Odisha '],
+        ['Punjab'],
+        ['Rajasthan'],
+        ['Tamil Nadu'],
+        ['Telengana'],
+        ['Sikkim '],
+        ['Uttar Pradesh'],
+        ['Meghalaya'],
+        ['Mizoram'],
+        ['Nagaland'],
+        
+     
+      ]
+  end
+end
